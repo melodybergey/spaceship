e81bd83ebb4814b76c9cd3e562c422325c3cf088
Melody Bergey <melodybbergey@gmail.com>
2021-05-04 20:33:02 -0400

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -43,7 +43,7 @@ ready(() => {
   function spaceshipTL(){
     let tl = gsap.timeline();
 
-    tl.to("#Rocket", {duration: 7, motionPath: {path: "#Line", align: "#Line", alignOrigin: [0,0]}});
+    tl.to("#Rocket", {duration: 7, motionPath: {path: "#Line", align: "#Line", alignOrigin: [0.5,0.5]}});
 
     //tl END
 
